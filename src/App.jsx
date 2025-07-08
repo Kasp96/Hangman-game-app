40a60ff5576db8202a1095642695388dfc7ab2cf
Kacper <kacperwach96@gmail.com>
2025-07-08 19:00:00 +0200

    repo init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import '../src/main.css';
+import '../src/styles/globals.css';
+import '../src/styles/theme.css';
+
+function App() {
+	return <>test</>;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+
+const router = createBrowserRouter([
+	{
+		element: <App />,
+		path: '/',
+	},
+]);
+
+createRoot(document.getElementById('root')).render(
+	<RouterProvider router={router} />
+);
